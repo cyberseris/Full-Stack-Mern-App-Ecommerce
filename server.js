adf851ff21765ae1dea8ef0279e3aaca3da6c30d
Skey <chenskey8@gmail.com>
2023-10-20 13:03:38 +0800

    cor 設定

--- server.js
+++ server.js
@@ -29,17 +29,23 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
-app.use(cors(
+/* app.use(cors(
     {
         origin: ["https://mern-stack-ecommerce-app-vercel-q6t2pdkzz-seriskeys-projects.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
-));
+)); */
 
 //rest api
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to ecommerce app MERN STACK Project</h1>")
+});
+
+app.get('/test', (req, res) => {
+    res.json({
+        message: "test work!!",
+    });
 });
 
 //port
